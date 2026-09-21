84d8e272bc6f012fbc8207458a9ef408b5bc3696
Anton Krylatov <serverbsvvv@gmail.com>
2026-09-21 11:08:44 +0300

    Centered text and add gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: (Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.pink, Colors.white],
+            ),
+          ),
+          child: Center(
+            child: Text("Hello world!"),
+          ),
+        ),
+      )),
+    ),
+  );
 }
